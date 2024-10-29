eb149a176ebbaf452d30a8b5855b529ef7e06ff8
Priscila Freitas Martins <168697328+FreitasPriscila@users.noreply.github.com>
2024-10-29 11:11:18 -0300

    Add files via upload

---
+++ Exercicio1/src/main/java/com/mycompany/exercicio1/Produto.java
@@ -0,0 +1,17 @@
+package com.mycompany.exercicio1;
+public class Produto{
+            String nome;
+            float preco;
+
+
+public Produto(String nome,float preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+public String getNome(){
+    return nome;
+}
+public float getPreco(){
+    return preco;
+}
+}
